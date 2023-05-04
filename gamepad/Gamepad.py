542b5e7e19f7e508e48381ced3ecd607412e22fc
BoeseKnight <lamashko.ilya@yandex.by>
2023-05-04 03:06:50 +0300

    GamepadHandler splitted into functions

--- gamepad/Gamepad.py
+++ gamepad/Gamepad.py
@@ -10,11 +10,4 @@ class Gamepad:
         pygame.joystick.init()
         cls.controller = pygame.joystick.Joystick(0)
         cls.controller.init()
-        print("Name" + str(cls.controller.get_name()))
-        print("Buttons" + str(cls.controller.get_numbuttons()))
-        print("Axes" + str(cls.controller.get_numaxes()))
-        print("Hats"+str(cls.controller.get_numhats()))
-        # while True:
-        #     for event in pygame.event.get():
-        #         if event.type == pygame.JOYBUTTONDOWN:
-        #             print(f'Gamepadd button {event.button} pressed')
+
--- gamepad/GamepadHandler.py
+++ gamepad/GamepadHandler.py
@@ -5,58 +5,57 @@ import pygame
 
 
 class GamepadHandler:
+    axis = {}
+    button = {}
+    hat = {}
+
     @classmethod
     def __initialization(cls):
-        axis = {}
-        button = {}
-        hat = {}
         Gamepad.setup()
         # Axes and buttons initialization
         for i in range(Gamepad.controller.get_numaxes()):
-            # print(Gamepad.controller.get_numaxes())
-            axis[i] = 0.0
+            cls.axis[i] = 0.0
         for i in range(Gamepad.controller.get_numbuttons()):
-            # print(Gamepad.controller.get_numbuttons())
-            button[i] = False
+            cls.button[i] = False
         for i in range(Gamepad.controller.get_numhats()):
-            hat[i] = (0, 0)
-        return axis, button, hat
+            cls.hat[i] = (0, 0)
+
+    @classmethod
+    def __handle_events(cls):
+        axis_event = None
+        button_event = None
+        gamepad_events = pygame.event.get()
+        for event in gamepad_events:
+            if event.type == pygame.JOYAXISMOTION:
+                cls.axis[event.axis] = round(event.value, 3)
+                if cls.axis[event.axis] != 0:
+                    axis_event = event.axis
+            if event.type == pygame.JOYBUTTONDOWN:
+                cls.button[event.button] = True
+                button_event = event.button
+            if event.type == pygame.JOYBUTTONUP:
+                cls.button[event.button] = False
+            if event.type == pygame.JOYHATMOTION:
+                cls.hat[event.hat] = event.value
+                button_event = GamepadDpad.get_direction(cls.hat[event.hat])
+        return axis_event, button_event
 
     @classmethod
     def run(cls):
-        axis, button, hat = cls.__initialization()
+        cls.__initialization()
         print("Press PS button to quit:")
         end = False
         while end is False:
             axis_event = None
             button_event = None
             while axis_event is None and button_event is None:
-                # Get events
-                gamepad_events = pygame.event.get()
+                # Get gamepad events
+                axis_event, button_event = cls.__handle_events()
 
-                for event in gamepad_events:
-                    if event.type == pygame.JOYAXISMOTION:
-                        axis[event.axis] = round(event.value, 3)
-                        if axis[event.axis] != 0:
-                            # print(f"Axis: {event.axis}   Value: {axis[event.axis]}")
-                            axis_event = event.axis
-                    if event.type == pygame.JOYBUTTONDOWN:
-                        button[event.button] = True
-                        button_event = event.button
-                        end = button[GamepadButtons.PS.value]
-                    if event.type == pygame.JOYBUTTONUP:
-                        button[event.button] = False
-                    if event.type == pygame.JOYHATMOTION:
-                        hat[event.hat] = event.value
-                        button_event = GamepadDpad.get_direction(hat[0])
+            end = cls.button[GamepadButtons.PS.value]
             if axis_event is not None:
-                action = GamepadCommand(axis_event, GamepadSticks(axis_event).name, axis[axis_event])
+                action = GamepadCommand(axis_event, GamepadSticks(axis_event).name, cls.axis[axis_event])
                 print(action)
-
             if button_event is not None:
                 action = GamepadCommand(button_event, GamepadButtons(button_event).name)
                 print(action)
-
-            # # Limited to 30 frames per second to make the display not so flashy
-            # clock = pygame.time.Clock()
-            # clock.tick(30)
